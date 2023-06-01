434341a1f260e766e17930298fd4a5de63d41736
codekid69 <bishtyash069@gmail.com>
2023-06-01 23:54:18 +0530

    File Structuring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AddProducts, Home, Cart, Product, NotFound,ConfirmOrder } from "./pages";
+import { Navbar,Loader,Item,ItemList,FilterData,Counter,Footer} from "./components";
+import './styles/home.css';
+import './styles/cart.css';
+import './styles/product.css';
+import './styles/addproduct.css';
+import './styles/notfound.css';
+import './styles/loader.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/add" element={<AddProducts />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/confirmorder" element={<ConfirmOrder/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
